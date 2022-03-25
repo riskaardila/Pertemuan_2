e6e30bad45050bd06e987dc9fae13daede0552a5
Riska Ardila <riskaardila35@gmail.com>
2022-03-25 15:46:00 +0700

    Percobaan Pemeriksaan Kondisi Null

---
+++ src/main/kotlin/28_Pemerikasaan_Kondisi_Null.kt
@@ -0,0 +1,8 @@
+fun main() {
+    //Conditions
+    var maybeNumber: Int? = 15
+    if (maybeNumber != null)
+        println(maybeNumber * 2)
+    else
+        println("The emtered value isn't a number")
+}
