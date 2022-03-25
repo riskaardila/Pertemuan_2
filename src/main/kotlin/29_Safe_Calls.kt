ae7278fb83c62a82dbb9bbf2e0c0254b33888bab
Riska Ardila <riskaardila35@gmail.com>
2022-03-25 15:46:23 +0700

    Percobaan Safe Calls

---
+++ src/main/kotlin/29_Safe_Calls.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // Safe Calling
+    // ?.Let
+    var maybeNumber: Int? = 15
+    maybeNumber?.let { println(it) }
+
+    // the ?. Chaining
+    //student?.teacher?.supervisor?.name = "Riska Ardila"
+}
